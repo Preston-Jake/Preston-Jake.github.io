a9f49822abc88509db003dc942027a0232e7c12a
JakePreston <prestonjak@outlook.com>
2020-01-26 10:23:51 -0600

    Updates

---
+++ precache-manifest.1f088e712daeecff9fe1516532e5d390.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53a6909ae6e3078a5bfc70b0932fdb3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69e7f0a825b63dd90f5c",
+    "url": "/static/css/main.c60ff1c8.chunk.css"
+  },
+  {
+    "revision": "32f4e488b2627ea1ba92",
+    "url": "/static/js/2.feb1ad95.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.feb1ad95.chunk.js.LICENSE"
+  },
+  {
+    "revision": "69e7f0a825b63dd90f5c",
+    "url": "/static/js/main.1d80d6f9.chunk.js"
+  },
+  {
+    "revision": "c9913191e565de632244",
+    "url": "/static/js/runtime-main.170f8412.js"
+  },
+  {
+    "revision": "a11587e153a7c691c78a5d9456f714e8",
+    "url": "/static/media/JakePrestonFun.a11587e1.png"
+  },
+  {
+    "revision": "5806938129207c4b3b890cc437f7f02d",
+    "url": "/static/media/JakePrestonResume.58069381.pdf"
+  },
+  {
+    "revision": "dc128602b7347ec63b99a8373ffd9492",
+    "url": "/static/media/Nushell.dc128602.jpg"
+  },
+  {
+    "revision": "a53e02edee86e852e292849a329ffd9e",
+    "url": "/static/media/Shire1.a53e02ed.png"
+  },
+  {
+    "revision": "c8fbfa2f0eb2de526acab0f3425d0e56",
+    "url": "/static/media/Shire2.c8fbfa2f.png"
+  },
+  {
+    "revision": "8a1a791f93b5d06271d5ebbb3b4d728c",
+    "url": "/static/media/Waiterly.8a1a791f.png"
+  },
+  {
+    "revision": "5673759972f75d2da676b0d8b5dc2746",
+    "url": "/static/media/WelcomeToNash.56737599.png"
+  },
+  {
+    "revision": "feb8ca2502933843414d4556ca3afc6b",
+    "url": "/static/media/brook-anderson-gTQbZXL417Q-unsplash.feb8ca25.jpg"
+  },
+  {
+    "revision": "c5ec29add9800468d7953a20c87bbc0d",
+    "url": "/static/media/browserify.c5ec29ad.png"
+  },
+  {
+    "revision": "f00f4fb99fa5abebfbd57491fe81d64a",
+    "url": "/static/media/c.f00f4fb9.png"
+  },
+  {
+    "revision": "7ffec13f75db6fdef2128f9663042ab6",
+    "url": "/static/media/css.7ffec13f.png"
+  },
+  {
+    "revision": "cac4d8e3b58d853cb6e1b374de845a13",
+    "url": "/static/media/docker.cac4d8e3.png"
+  },
+  {
+    "revision": "970f12067f61fda90f8aa177b3de9518",
+    "url": "/static/media/firebase.970f1206.png"
+  },
+  {
+    "revision": "81725d40fdfb42e2c95f8ce418615983",
+    "url": "/static/media/git.81725d40.png"
+  },
+  {
+    "revision": "b5d42b900b60a8f09a76a252a282e534",
+    "url": "/static/media/github.b5d42b90.png"
+  },
+  {
+    "revision": "2ac410f9314b6df14f7c949851b8e9ee",
+    "url": "/static/media/grunt.2ac410f9.png"
+  },
+  {
+    "revision": "74680ca80aff811398ca6e5c0ba3449b",
+    "url": "/static/media/html.74680ca8.png"
+  },
+  {
+    "revision": "a6797e56f16abdefb98b647a4b073de2",
+    "url": "/static/media/johny-goerend-Oz2ZQ2j8We8-unsplash.a6797e56.jpg"
+  },
+  {
+    "revision": "8783ebae2bf1cf2f380ad214a1f510cd",
+    "url": "/static/media/js.8783ebae.png"
+  },
+  {
+    "revision": "2d897c6e586c02776071a145ec358192",
+    "url": "/static/media/jsx.2d897c6e.png"
+  },
+  {
+    "revision": "67740297654b5957ab08a854225165d9",
+    "url": "/static/media/kevin-horvat-Pyjp2zmxuLk-unsplash.67740297.jpg"
+  },
+  {
+    "revision": "eef08a16be558883244d91db71a1296a",
+    "url": "/static/media/nashville.eef08a16.jpg"
+  },
+  {
+    "revision": "52babf49fcfc71790b5dd4dfce8d69fb",
+    "url": "/static/media/nodejs.52babf49.png"
+  },
+  {
+    "revision": "4426b7a9bd82fdff6323d370cc5d0aa6",
+    "url": "/static/media/php.4426b7a9.png"
+  },
+  {
+    "revision": "f34276aae7328818885e74688c3e26a5",
+    "url": "/static/media/react.f34276aa.png"
+  },
+  {
+    "revision": "ff9a862744fcfa106cc36ce89b32aaed",
+    "url": "/static/media/ruby.ff9a8627.png"
+  },
+  {
+    "revision": "4b59d1c7683ddda6fb2124647ea3ed6d",
+    "url": "/static/media/wordpress.4b59d1c7.png"
+  }
+]);
