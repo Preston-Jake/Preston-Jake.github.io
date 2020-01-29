d21dadd0fe94ff36dc074073146c03249ac5b698
JakePreston <prestonjak@outlook.com>
2020-01-29 12:26:51 -0600

    styling and navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-scroll";
 export default function Navbar() {
   return (
     <div class="Navbar">
-      <Link to="home" spy={true} smooth={true} duration={500}>
+        <Link to="home" spy={true} smooth={true} duration={500}>
         Home
       </Link>
       <Link to="about" spy={true} smooth={true} duration={500}>
