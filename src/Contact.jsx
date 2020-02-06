dc8fdf38fb7aadcd411a8dad30c6b9a4a96ac648
JakePreston <prestonjak@outlook.com>
2020-02-06 13:56:16 -0600

    update styling

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import "./Contact.css";
 import resume from "./images/JakePrestonResume.pdf";
 import { Element } from "react-scroll";
+import Zoom from 'react-reveal/Zoom';
+
 export default function Contact() {
   return (
     <Element name="contact" class="Contact-background">
       <div class="Contact-overlay">
         <div class="Contact-content">
+
           <svg
             class="Contact-icon"
             width="100"
--- src/Profile.jsx
+++ src/Profile.jsx
@@ -2,18 +2,25 @@ import React from "react";
 import "./Profile.css";
 import JakePrestonFun from "./images/JakePrestonFun.png";
 import { Element } from "react-scroll";
+import Fade from "react-reveal/Fade";
+import LightSpeed from "react-reveal/LightSpeed";
+
 export default function Profile() {
   return (
     <Element name="home" class="Profile-background">
       <div class="Profile-overlay">
         <div class="Profile-content">
           <div class="Profile-heading">
-            <h1>Jake Preston</h1>
-            <h2>Full Stack Developer</h2>
-            <p>
-              A burrito eating, rock wall climbing, food formulating, full stack
-              developer with an appetite for learning.
-            </p>
+            <Fade top big>
+              <h1>Jake Preston</h1>
+              <h2>Full Stack Developer</h2>
+            </Fade>
+            <Fade right>
+              <p>
+                A burrito eating, rock wall climbing, food formulating, full
+                stack developer with an appetite for learning.
+              </p>
+            </Fade>
           </div>
           <div class="Profile-image-wrapper">
             <img
