9c1c970beae0c3796512084b19a589e66e5eaabe
JakePreston <prestonjak@outlook.com>
2020-02-07 13:09:44 -0600

    save profile css

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -3,35 +3,30 @@ import "./Profile.css";
 import JakePrestonFun from "./images/JakePrestonFun.png";
 import { Element } from "react-scroll";
 import Fade from "react-reveal/Fade";
-import LightSpeed from "react-reveal/LightSpeed";
+import Slide from "react-reveal/Slide";
+
 
 export default function Profile() {
   return (
-    <Element name="home" class="Profile-background">
-      <div class="Profile-overlay">
-        <div class="Profile-content">
-          <div class="Profile-heading">
-            <Fade top big>
-              <h1>Jake Preston</h1>
-              <h2>Full Stack Developer</h2>
-            </Fade>
-            <Fade right>
-              <p>
-                A burrito eating, rock wall climbing, food formulating, full
-                stack developer with an appetite for learning.
-              </p>
-            </Fade>
-          </div>
-          <div class="Profile-image-wrapper">
-            <img
-              class="Profile-image"
-              src={JakePrestonFun}
-              alt=""
-              srcset={JakePrestonFun}
-            />
-          </div>
-        </div>
-      </div>
+    <Element name="home" class="Profile-wrapper">
+      
+      <Slide top big>
+        <h1>Jake Preston</h1>
+        <h2>Full Stack Developer</h2>
+      </Slide>
+      <Slide bottom big>
+        <p>
+          A burrito eating, rock wall climbing, food formulating, full stack
+          developer with an appetite for learning.
+        </p>
+      </Slide>
+      
+      <img
+        class="Profile-image"
+        src={JakePrestonFun}
+        alt=""
+        srcset={JakePrestonFun}
+      />
     </Element>
   );
 }
