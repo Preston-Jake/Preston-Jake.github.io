55c11cc4b12a9cb400b025a635b41060104b8c39
JakePreston <prestonjak@outlook.com>
2020-02-20 11:35:38 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f048533e703d84dc3d795160301719b1.js"
+  "/precache-manifest.19de65d5f275025032205bea74cc9427.js"
 );
 
 self.addEventListener('message', (event) => {
